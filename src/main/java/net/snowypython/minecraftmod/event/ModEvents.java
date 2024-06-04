eb1544b8de22c3455ac71e2bfeb68bd2749a971f
SnowyPython <SnowyPython@vk.com>
2024-06-04 11:58:34 +0500

    Металлург
    Добавил предметы для торговли

--- src/main/java/net/snowypython/minecraftmod/event/ModEvents.java
+++ src/main/java/net/snowypython/minecraftmod/event/ModEvents.java
@@ -18,13 +18,94 @@ import java.util.List;
 public class ModEvents {
     @SubscribeEvent
     public static void addCustomTrades(VillagerTradesEvent event) {
+        //metallurgist
         if(event.getType() == ModVillagers.METALLURGIST.get()) {
             Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
-            ItemStack stack = new ItemStack(ModItems.TIN_INGOT.get(), 2);
+            ItemStack stack = new ItemStack(Items.COPPER_INGOT, 2);
             int villagerLevel = 1;
 
             trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
                     new ItemStack(Items.EMERALD, 1),
+                    stack,10,8,0.02F));
+        }
+
+        if(event.getType() == ModVillagers.METALLURGIST.get()) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(Items.IRON_INGOT, 2);
+            int villagerLevel = 1;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 1),
+                    stack,10,8,0.02F));
+        }
+
+        if(event.getType() == ModVillagers.METALLURGIST.get()) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(ModItems.TIN_INGOT.get(), 2);
+            int villagerLevel = 2;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 1),
+                    stack,10,8,0.02F));
+        }
+
+        if(event.getType() == ModVillagers.METALLURGIST.get()) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(ModItems.ALUMINUM_INGOT.get(), 2);
+            int villagerLevel = 2;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 1),
+                    stack,10,8,0.02F));
+        }
+
+        if(event.getType() == ModVillagers.METALLURGIST.get()) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(ModItems.LEAD_INGOT.get(), 1);
+            int villagerLevel = 2;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 1),
+                    stack,10,8,0.02F));
+        }
+
+        if(event.getType() == ModVillagers.METALLURGIST.get()) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(ModItems.SILVER_INGOT.get(), 1);
+            int villagerLevel = 3;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 4),
+                    stack,10,8,0.02F));
+        }
+
+        if(event.getType() == ModVillagers.METALLURGIST.get()) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(Items.GOLD_INGOT, 1);
+            int villagerLevel = 3;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 4),
+                    stack,10,8,0.02F));
+        }
+
+        if(event.getType() == ModVillagers.METALLURGIST.get()) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(ModItems.PLATINUM_INGOT.get(), 1);
+            int villagerLevel = 4;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 8),
+                    stack,10,8,0.02F));
+        }
+
+        if(event.getType() == ModVillagers.METALLURGIST.get()) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(ModItems.TITANIUM_INGOT.get(), 1);
+            int villagerLevel = 4;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 8),
                     stack,10,8,0.02F));
         }
     }
