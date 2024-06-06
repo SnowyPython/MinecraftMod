aee44aa09617e2ccadcc2df9a8f925149a927de7
SnowyPython <SnowyPython@vk.com>
2024-06-06 20:01:04 +0500

    Горячая клавиша
    Пока что лишь заготовка

---
+++ src/main/java/net/snowypython/minecraftmod/event/ClientEvents.java
@@ -0,0 +1,31 @@
+package net.snowypython.minecraftmod.event;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.snowypython.minecraftmod.MinecraftMod;
+import net.snowypython.minecraftmod.util.KeyBinding;
+
+public class ClientEvents {
+    @Mod.EventBusSubscriber(modid = MinecraftMod.MOD_ID, value = Dist.CLIENT)
+    public static class ClientForgeEvents {
+        @SubscribeEvent
+        public static void onKeyInput(InputEvent.Key event) {
+            if(KeyBinding.DRINKING_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("pressed a key!"));
+            }
+        }
+    }
+
+    @Mod.EventBusSubscriber(modid = MinecraftMod.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModBusEvents {
+        @SubscribeEvent
+        public static void onKeyRegister(RegisterKeyMappingsEvent event) {
+            event.register(KeyBinding.DRINKING_KEY);
+        }
+    }
+}
